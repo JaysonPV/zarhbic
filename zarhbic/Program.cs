02fea39508bdcd9dd9f9411fb2db0e589f1cd3c9
Luffy <jaysonpvaldemar@gmail.com>
2024-01-18 22:42:22 +0100

    Ajoutez des fichiers projet.

---
+++ zarhbic/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+
+class RPNCalculator
+{
+    static void Main()
+    {
+        string expression = "5 3 + 8 * 0 /";
+        double result = EvaluateRPN(expression);
+        Console.WriteLine($"Le résultat de l'expression RPN est : {result}");
+    }
+
+    static double EvaluateRPN(string expression)
+    {
+        Stack<double> stack = new Stack<double>();
+        string[] tokens = expression.Split(' ');
+
+        foreach (string token in tokens)
+        {
+            if (double.TryParse(token, out double number))
+            {
+                stack.Push(number);
+            }
+            else if (IsOperator(token))
+            {
+                double operand2 = stack.Pop();
+                double operand1 = stack.Pop();
+                double result = PerformOperation(token, operand1, operand2);
+                stack.Push(result);
+            }
+            else
+            {
+                throw new ArgumentException($"Token non valide : {token}");
+            }
+        }
+
+        return stack.Pop();
+    }
+
+    static bool IsOperator(string token)
+    {
+        return token == "+" || token == "-" || token == "*" || token == "/";
+    }
+
+    static double PerformOperation(string op, double x, double y)
+    {
+        switch (op)
+        {
+            case "+":
+                return x + y;
+            case "-":
+                return x - y;
+            case "*":
+                return x * y;
+            case "/":
+                if (y == 0)
+                {
+                    throw new DivideByZeroException("Division par zéro.");
+                }
+                return x / y;
+            default:
+                throw new ArgumentException($"Opérateur non valide : {op}");
+        }
+    }
+}
